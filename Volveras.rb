cfb00b30d3c5f3d86bbb34e680737ba3dfcc159d
Thomas Holcomb <holcomb@nytimes.com>
2014-05-26 17:55:01 -0400

    add remainder files

---
+++ Volveras.rb
@@ -0,0 +1,68 @@
+Volveras
+yo lloro como el arroyo
+al llegar el rio
+yo muero
+y muero como el rio
+al llegar el agua
+de amor yo muero
+yo hice lo que pude
+te ame como sabia
+y al final de todo
+tu amor yo no tenia
+me vuelvo rio y muero
+cerro la puerta
+sin decir adios
+nunca volvi a verla
+nunca mas volvio
+como yo te quise
+nadie, nadie te ha querido
+insensata mia por que te has ido
+y me dejaste sola como el mar
+yo vivo como el aire
+libre pero sin saber a donde va
+y nadie nadie nadie te ha querido
+insensata loca por que te has ido
+tu volveras
+y cuando tu regreses amor
+veras como alguien quiso ocupar
+mi pobre corazón
+por ti
+y ya veras
+como tu a mi me pides perdon
+y yo que ya estoy loca de amor
+yo voy y te perdono
+no eran tan falsas
+aquellas mentiras
+ni tan verdaderas
+tus verdades favoritas
+no fueron tan callados aquellos silencios
+no fueron tan malos algunos momentos
+si ahora te marchas
+vete para siempre
+no te des la vuelta que las vueltas siempre duelen
+y abre la ventana
+que da al paraiso
+olvidame si puedes que yo no he podido
+tu volveras
+y cuando tu regreses amor
+veras como alguien quiso ocupar
+mi pobre corazon
+por ti
+y ya veras
+como tu a mi me pides perdon
+y yo que ya estoy loca de amor
+yo voy y te perdono
+yo te perdono
+y tu, tu volveras
+y cuando tu regreses amor
+veras como alguien quiso ocupar
+mi pobre corazon
+por ti
+y ya veras
+como tu a mi me pides perdon
+y yo que ya estoy loca de amor
+yo voy
+yo voy y te perdono
+
+(Gracias a klaw por esta letra)
+</div>
